b19afafa86b31b5e2dc3d9abb9c8bbaf569c824a
ambikaarumugam <ambikaarumugams@gmail.com>
2025-03-17 00:02:39 +1100

    Implementing Annotations

--- TurnUpPortal/Tests/TM_Tests.cs
+++ TurnUpPortal/Tests/TM_Tests.cs
@@ -4,28 +4,44 @@ using TurnUpPortal.Utilities;
 
 namespace TurnUpPortal.Tests
 {
-    [TestFixture]
-    public class TM_Tests:CommonDriver
 
+    // [TestFixture]                                     -  Non -Parameterized TestFixture
+
+    [TestFixture("Chrome", "http://horse.industryconnect.io/", "hari", "123123")]        // Parameterized - DDT
+    [TestFixture("Edge", "http://horse.industryconnect.io/", "hari", "123123")]
+    //[TestFixture("Firefox", "http://horse.industryconnect.io/", "hari", "123123")]    //install firefox
+
+    public class TM_Tests:CommonDriver
     {
+        private readonly string _username;
+        private readonly string _password;
+
+        public TM_Tests(string browser, string url,string username, string password) : base(browser, url)
+        {
+            _username = username;
+            _password = password;
+        }
+
+        //[TestCase("Hari", "123123")]                                        - passed
         private void Login()
         {
             //Login page object initialization and definition
             LoginPage loginPageObj = new LoginPage(driver);
-            loginPageObj.Login("Hari", "123123");
+            loginPageObj.Login(_username,_password);
 
             //Assertion
             string loggedInMsg = loginPageObj.GetLoginText();
             Assert.That(loggedInMsg == "Hello hari!", "Actual and Expected result don't match!");
         }
 
-        [Test]
+        [Test, Order(1)]
+        [Category("smoke")]                    //Category to group the tests
         public void Login_Test()
         {
             Login();
         }
 
-        [Test]
+        [Test, Order(2)]
         public void Home_Test()
         {
             Login();
@@ -38,52 +54,53 @@ namespace TurnUpPortal.Tests
             Assert.That(createNewMsgDisplayed == true, "Actual and Expected are not matched!");
         }
 
-        [Test]
-        public void CreateTime_Test()
+        [TestCase("A_030"), Order(3)]
+        public void CreateTime_Test(string code)
         {
              Home_Test();
             //Time and Materials page object initialization and definition
             TimeAndMaterialPage timeAndMaterialPageObj = new TimeAndMaterialPage(driver);
-            timeAndMaterialPageObj.CreateRecord("A_025","Automation","4000");
+            timeAndMaterialPageObj.CreateRecord(code,"Automation","4000");
 
             //Assertion
             string newCodeTxt = timeAndMaterialPageObj.GetNewCode();
             string newDescriptionTxt = timeAndMaterialPageObj.GetNewDescription();
             string newPriceTxt = timeAndMaterialPageObj.GetNewPrice();
 
-            Assert.That(newCodeTxt == "A_025", "Actual and Expected results don't match!");
+            Assert.That(newCodeTxt == code, "Actual and Expected results don't match!");
             Assert.That(newDescriptionTxt == "Automation", "Actual and Expected results don't match!");
             Assert.That(newPriceTxt == "$4,000.00", "Actual and Expected results don't match!");
         }
 
-        [Test]
+        [Test, Order(4)]
         public void EditTime_Test()
         {
-            CreateTime_Test();
+            string code = "A_050";
+            CreateTime_Test(code);
             TimeAndMaterialPage timeAndMaterialPageObj = new TimeAndMaterialPage(driver);
-            timeAndMaterialPageObj.Edit("A_050", "Test");
+            timeAndMaterialPageObj.Edit(code, "Test");
 
             //Assertion
             string editedCodeTxt = timeAndMaterialPageObj.GetEditedCode();
             string editedDescriptionTxt = timeAndMaterialPageObj.GetEditedDescription();
 
-            Assert.That(editedCodeTxt == "A_050", "Actual and Expected don't match!");
+            Assert.That(editedCodeTxt == code, "Actual and Expected don't match!");
             Assert.That(editedDescriptionTxt == "Test", "Actual and Expected don't match!");
         }
      
-        [Test]
+        [Test, Order(5)]
+        [Category("Regression")]
         public void DeleteTime_Test()
         {
-            CreateTime_Test();
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+            string code = $"A_{randomNumber}";
+            CreateTime_Test(code);
             TimeAndMaterialPage timeAndMaterialPageObj = new TimeAndMaterialPage(driver);
             timeAndMaterialPageObj.Delete();
-                                                                                                                                                //check
-            //Assertion
+                                                                                                                                                
             string lastCodeTxt = timeAndMaterialPageObj.GetLastCode();
-            string lastDescriptionTxt = timeAndMaterialPageObj.GetLastDescription();
-
-            Assert.That(lastCodeTxt != "A_050", "Actual and Expected both are matching!");
-            Assert.That(lastDescriptionTxt != "Test", "Actual and Expected both are matching!");
+            Assert.That(lastCodeTxt,Does.Not.Contain(code), "Record was not deleted!");
         }
 
     }
--- TurnUpPortal/Utilities/CommonDriver.cs
+++ TurnUpPortal/Utilities/CommonDriver.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
 
 
 namespace TurnUpPortal.Utilities
@@ -8,15 +10,40 @@ namespace TurnUpPortal.Utilities
     public class CommonDriver
     {
         public static IWebDriver driver;
+        public string browser;
+        public string url;
+
+        //Constructor 1
+        public CommonDriver(string browser, string url)
+        {
+            this.browser = browser;
+            this.url = url;
+        }
+
 
         [SetUp]
         public void SetUpSteps()
         {
             //Open the chrome browser
-            driver = new ChromeDriver();
+           // driver = new ChromeDriver();
+           switch(browser.ToLower())
+            {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    throw new ArgumentException($"Browser '{browser}' is not supported");
+
+            }
 
             //Launch TurnUp Portal
-            driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+            driver.Navigate().GoToUrl(url);
             driver.Manage().Window.Maximize();
         }
 
